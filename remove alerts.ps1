a99c30220f39ea5d2112a74db684eede42f2ad65
Mohamed Sabeq <msabeq@outlook.com>
2024-06-25 17:25:35 +0300

    update the remove alerts.ps1

--- remove alerts.ps1
+++ remove alerts.ps1
@@ -2,17 +2,32 @@
 $resourceGroupName = "vf-core-UK-resources-rg"
 
 # Get all metric alert rules in the specified resource group
-$alertRules = Get-AzMetricAlertRuleV2 -ResourceGroupName $resourceGroupName
+$MetricAlertV2 = Get-AzMetricAlertRuleV2 -ResourceGroupName $resourceGroupName
 
-foreach ($rule in $alertRules) {
+foreach ($ruleMatric in $MetricAlertV2) {
     # Display the alert rule being deleted
-    Write-Output "Deleting alert rule: $($rule.Name)"
+    Write-Output "Deleting alert rule: $($ruleMatric.Name)"
 
     # Delete the alert rule
     try {
-        Remove-AzMetricAlertRuleV2 -ResourceGroupName $resourceGroupName -Name $rule.Name 
-        Write-Output "Deleted alert rule: $($rule.Name)"
+        Remove-AzMetricAlertRuleV2 -ResourceGroupName $resourceGroupName -Name $ruleMatric.Name 
+        Write-Output "Deleted alert rule: $($ruleMatric.Name)"
     } catch {
-        Write-Output "Failed to delete alert rule $($rule.Name): $_"
+        Write-Output "Failed to delete alert rule $($ruleMatric.Name): $_"
+    }
+}
+
+$ActivityLogAlert = Get-AzActivityLogAlert -ResourceGroupName $resourceGroupName
+
+foreach ($ruleLogAlert in $ActivityLogAlert) {
+    # Display the alert rule being deleted
+    Write-Output "Deleting alert rule: $($ruleLogAlert.Name)"
+
+    # Delete the alert rule
+    try {
+        Remove-AzActivityLogAlert -ResourceGroupName $resourceGroupName -Name $ruleLogAlert.Name 
+        Write-Output "Deleted alert rule: $($ruleLogAlert.Name)"
+    } catch {
+        Write-Output "Failed to delete alert rule $($ruleLogAlert.Name): $_"
     }
 }
