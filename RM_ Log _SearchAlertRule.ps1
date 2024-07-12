46059d0afe17da61596a5fff16b8c67adfe78cfa
msabeq94 <msabeq@outlook.com>
2024-07-12 16:15:41 +0300

    Add RM_RG

--- RM_ Log _SearchAlertRule.ps1
+++ RM_ Log _SearchAlertRule.ps1
@@ -1,20 +1,16 @@
-$subscriptionID = "c3323cc6-1939-4b36-8714-86504bbb8e4b"
+$subscriptionId = "c3323cc6-1939-4b36-8714-86504bbb8e4b"
+$resourceGroupToRemove = "VF-CloudMonitoringv4"
+$resourceManagementScope = "/subscriptions/$($subscriptionId)/resourceGroups/$($resourceGroupToRemove)"
 
+$policyAssignmentsInScope = Get-AzPolicyAssignment  -scope $resourceManagementScope 
 
-$rgtoRM = "VF-CloudMonitoringv4"
-
-$RMScope = "/subscriptions/$($subscriptionID)/resourceGroups/$($rgtoRM)"
-
-$policyAssignments = Get-AzPolicyAssignment  -scope $RMScope 
-
-foreach ($policyAssignment in $policyAssignments) {
+foreach ($policyAssignment in $policyAssignmentsInScope) {
     Remove-AzPolicyAssignment -id $policyAssignment.id  -Force
     Write-Output "Removed policy assignment: $($policyAssignment.Name)"
 }
 
-
-$policyDefinitions = get-AzPolicyDefinition | Where-Object { $_.Name -like "vf-core-cm-*-$rgtoRM"}
-foreach ($policyDefinition in $policyDefinitions) {
+$policyDefinitionsInScope = get-AzPolicyDefinition | Where-Object { $_.Name -like "vf-core-cm-*-$resourceGroupToRemove"}
+foreach ($policyDefinition in $policyDefinitionsInScope) {
     Remove-AzPolicyDefinition -Name $policyDefinition.name -Force
     Write-Output "Removed policy Definition: $($policyDefinition.Name)"
 }
